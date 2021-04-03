c5bec064c9cdf3b7136c0744688d86441e81b9ca
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-03 16:47:46 +0300

    Php, laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Route::get('/', function () {return view('pages.index'); }) ->name('index');
+Route::get('/resume', function () {return view('pages.resume'); }) ->name('resume');
+Route::get('/portfolio', function () {return view('pages.portfolio'); }) ->name('portfolio');
+Route::get('/blog', function () {return view('pages.blog'); }) ->name('blog');
+Route::get('/contact', function () {return view('pages.contact'); }) ->name('contact');
+
